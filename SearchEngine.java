e0a08d8d990f1360e4695e7f85156b7d86234b25
sasankap742 <78668680+sasankap742@users.noreply.github.com>
2022-10-20 08:07:51 -0700

    addSearchEngine

---
+++ SearchEngine.java
@@ -0,0 +1,14 @@
+import javax.xml.ws.handler.Handler;
+
+public class SearchEngine implements URLHandler {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
